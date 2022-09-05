6439adff1cdf75a9d15b6a72410752ea27a7952d
Maksimchik36 <Peltek1985@gmail.com>
2022-09-05 22:24:26 +0300

    clone and change settings

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+// import { useState, useEffect } from 'react';
 
 function Inputs() {
   const [userCurrencyFrom, setUserCurrencyFrom] = useState('');
